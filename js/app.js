9615924289b6bab9d5c52f8297bc6aa2ea38151c
michojekunle <michojekunle1@gmail.com>
2022-07-06 01:18:54 +0100

    working on background music

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
-const searchKeyword = document.querySelector()
+const searchKeyword = document.querySelector('#search');
+const postContainer = document.querySelector('#post-container');
+const loading = document.querySelector('#loader');
+// const seachKeyword = document.querySelector('post-container');
+
