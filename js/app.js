400447225bd09e7b5f62dd34c7bee8d318a73ff7
michojekunle <michojekunle1@gmail.com>
2022-07-06 03:01:03 +0100

    sweetchious

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 const searchKeyword = document.querySelector('#search');
 const postContainer = document.querySelector('#post-container');
 const loading = document.querySelector('#loader');
-// const seachKeyword = document.querySelector('post-container');
+
+const getPosts = async () => {
+    
+}
+
 
